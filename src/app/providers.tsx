92085ab8e0dddc416fd12278b0ce32b13c8223e8
Zak Mayfield <zakmayfield@gmail.com>
2023-04-17 17:24:12 -0400

    remove extend theme until ready to implement theme customization

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,12 +1,11 @@
 'use client';
 import { CacheProvider } from '@chakra-ui/next-js';
 import { ChakraProvider } from '@chakra-ui/react';
-import { extendTheme } from '@chakra-ui/react';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <CacheProvider>
-      <ChakraProvider theme={extendTheme}>{children}</ChakraProvider>
+      <ChakraProvider>{children}</ChakraProvider>
     </CacheProvider>
   );
 }
