adf20cadb3d0dba33dbbcae3dd80c6ea7631effd
Zak Mayfield <zakmayfield@gmail.com>
2023-04-17 19:50:57 -0400

    updated nav with styles and chasers images

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,29 +1,62 @@
 'use client';
 import { List, ListItem, ListIcon, Flex, Box, Text } from '@chakra-ui/react';
+import Link from 'next/link';
+import Image from 'next/image';
+import { usePathname } from 'next/navigation';
+import logo from '../shared/assets/chasers-juice-logo.png';
+
+
+
+const navigationItems = [
+  { label: 'Home', route: '/' },
+  { label: 'About Us', route: '/about-us' },
+  { label: 'Products', route: '/products' },
+  { label: 'F.A.Q.', route: '/faq' },
+  { label: 'Contact Us', route: '/contact-us' },
+];
 
 export default function Nav() {
+  const pathname = usePathname();
+
   return (
-    <Flex px={20}>
+    <Flex px={12} borderBottom='1px' borderColor='blackAlpha.300'>
       <Box flex='1' display='flex'>
-        <Box p={48}>🧃</Box>
+        <Image src={logo} alt='Chasers Fresh Juice Logo' width={150} />
       </Box>
 
       <List
         display='flex'
         alignItems='center'
-        gap={20}
+        gap={8}
         flex='2'
         justifyContent='flex-end'
         textTransform='uppercase'
+        fontSize='xs'
+        letterSpacing={1}
+        color='blackAlpha.500'
+        fontWeight='medium'
       >
-        <ListItem>
-          <Text fontSize='2xl' color="red">Home</Text>
-        </ListItem>
-        <ListItem fontSize='2xl'>About Us</ListItem>
-        <ListItem>Products</ListItem>
-        <ListItem>F.A.Q.</ListItem>
-        <ListItem>Contact Us</ListItem>
-        <ListItem>Order</ListItem>
+        {navigationItems.map((item) => (
+          <ListItem
+            key={item.route}
+            color={pathname === item.route ? 'chakra-body-text' : ''}
+            _hover={{ color: 'gray.600' }}
+          >
+            <Link href={item.route}>{item.label}</Link>
+          </ListItem>
+        ))}
+        <Link href='.'>
+          <Box
+            as='span'
+            color='white'
+            bg='red.400'
+            fontWeight='bold'
+            p={2.5}
+            _hover={{ bg: 'red.600' }}
+          >
+            Order
+          </Box>
+        </Link>
       </List>
     </Flex>
   );
