1a8f4c1cc2be19e7d874996eb0519b1a2fd00fd6
Zak Mayfield <zakmayfield@gmail.com>
2023-04-17 17:25:47 -0400

    self close head tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <head></head>
+      <head />
       <Box as='body'>
         <Nav />
         <Providers>{children}</Providers>
