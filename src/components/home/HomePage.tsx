ff62a9b14d22c142f5a4fef268d74df2ba2c0517
Zak Mayfield <zakmayfield@gmail.com>
2023-04-21 13:40:51 -0400

    changed name of home page component to HomePage

--- src/components/home/HomePage.tsx
+++ src/components/home/HomePage.tsx
@@ -1,13 +1,11 @@
 'use client';
 import { Box } from '@chakra-ui/react';
 import HomeHero from './HomeHero';
-import { ContentContainer } from '@/shared/components';
 
-export default function HomeContainer() {
+export default function HomePage() {
   return (
     <Box bg='green.200' m='0' p='0' w='full'>
       <HomeHero />
-      <ContentContainer></ContentContainer>
     </Box>
   );
 }
