bfb96129fbcd25d3d2c74fc93e013e3545c58708
Dani Mayfield <dani@neon.cc>
2023-04-24 16:44:52 -0400

    style: update home statement style

--- src/components/home/HomeStatement.tsx
+++ src/components/home/HomeStatement.tsx
@@ -1,4 +1,5 @@
-import { Box, Heading, Text } from '@chakra-ui/react';
+import { Box, Heading, Text, Icon } from '@chakra-ui/react';
+import { AiFillHeart } from 'react-icons/ai';
 import { ContentContainer } from '@/shared/components';
 
 export default function HomeStatement() {
@@ -10,17 +11,23 @@ export default function HomeStatement() {
         pt='28'
         pb='28'
       >
-        <Heading as='h2' fontSize='4xl'>
-          Freshly Pressed with ♥
+        <Heading
+          as='h2'
+          fontSize='4xl'
+          display='flex'
+          w='full'
+          justifyContent='center'
+        >
+          Freshly Pressed with{' '}
+          <Icon as={AiFillHeart} color='primary.600' ml='2' />
         </Heading>
-        <Text mt='8' mb='12' fontWeight='light'>
+        <Text my='8' fontWeight='light'>
           With specially crafted recipes and a focus on consistency, the Chasers
           Juice team works daily to create delicious, 100% freshly pressed
-          juice. And some extra filler text just to round out this paragraph,
-          thank you.
+          juice.
         </Text>
         <Text fontWeight='light' letterSpacing='wider'>
-          We hope to see you around
+          We hope to see you soon!
         </Text>
       </ContentContainer>
     </Box>
