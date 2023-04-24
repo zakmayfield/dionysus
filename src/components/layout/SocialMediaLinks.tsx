26ead0933bdc3f7739b4bc724c680b2739b218f9
Dani Mayfield <dani@neon.cc>
2023-04-24 18:09:05 -0400

    chore: use whileHover property instead

--- src/components/layout/SocialMediaLinks.tsx
+++ src/components/layout/SocialMediaLinks.tsx
@@ -1,15 +1,9 @@
-import { Box, BoxProps, Flex } from '@chakra-ui/react';
+import { Box, Flex } from '@chakra-ui/react';
 import Link from 'next/link';
-import { motion } from 'framer-motion';
 import { FaFacebookF, FaInstagram } from 'react-icons/fa';
-import { useState } from 'react';
-
-export type MotionBoxProps = Omit<BoxProps, 'transition'>;
-export const MotionBox = motion<MotionBoxProps>(Box);
+import { MotionBox } from '@/shared/components';
 
 export const SocialMediaLinks = () => {
-  const [fbHover, setFbHover] = useState(false);
-  const [igHover, setIgHover] = useState(false);
   return (
     <Flex justifyContent='space-between'>
       <Link
@@ -19,18 +13,11 @@ export const SocialMediaLinks = () => {
         <Box
           border='1px solid black'
           display='inline-block'
-          p='2.5'
           transition='ease'
           transitionDuration='200ms'
           _hover={{ color: 'primary.600' }}
-          onMouseEnter={() => setFbHover(true)}
-          onMouseLeave={() => setFbHover(false)}
         >
-          <MotionBox
-            animate={{
-              scale: fbHover ? 1.2 : undefined,
-            }}
-          >
+          <MotionBox whileHover={{ scale: 1.2 }} p='2.5'>
             <FaFacebookF />
           </MotionBox>
         </Box>
@@ -42,18 +29,11 @@ export const SocialMediaLinks = () => {
         <Box
           border='1px solid black'
           display='inline-block'
-          p='2.5'
           transition='ease'
           transitionDuration='200ms'
           _hover={{ color: 'primary.600' }}
-          onMouseEnter={() => setIgHover(true)}
-          onMouseLeave={() => setIgHover(false)}
         >
-          <MotionBox
-            animate={{
-              scale: igHover ? 1.2 : undefined,
-            }}
-          >
+          <MotionBox whileHover={{ scale: 1.2 }} p='2.5'>
             <FaInstagram />
           </MotionBox>
         </Box>
