3125e047dc23eb0e205b42b39f4fde8fbc76becd
Zak Mayfield <zakmayfield@gmail.com>
2023-04-26 13:59:54 -0400

    remove console log

--- src/components/home/InstagramGallery.tsx
+++ src/components/home/InstagramGallery.tsx
@@ -3,5 +3,7 @@ import { useGalleryContext } from '@/context/GalleryContext';
 export const InstagramGallery = () => {
   const { data } = useGalleryContext();
 
+  console.log(data);
+
   return <div>Instagram Gallery</div>;
 };
