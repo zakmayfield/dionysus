41da24507b45e14075a3195af362f55dc50a3506
Zak Mayfield <zakmayfield@gmail.com>
2023-04-28 08:23:54 -0400

    added swiper for instagram gallery, base styles

--- src/components/home/InstagramGallery.tsx
+++ src/components/home/InstagramGallery.tsx
@@ -1,7 +1,41 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper';
+import { Box } from '@chakra-ui/react';
 import { useGalleryContext } from '@/context/GalleryContext';
+import './swiper.css';
+import 'swiper/css';
+import 'swiper/swiper-bundle.css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 export const InstagramGallery = () => {
   const { data } = useGalleryContext();
 
-  return <div>Instagram Gallery</div>;
+  return (
+    <>
+      <Swiper
+        slidesPerView={3}
+        spaceBetween={5}
+        pagination={{
+          clickable: true,
+          dynamicBullets: true,
+        }}
+        modules={[Pagination]}
+      >
+        {data.map((post) => (
+          <SwiperSlide key={post.id}>
+            {' '}
+            <Box
+              bgPosition='center'
+              bgRepeat='no-repeat'
+              bgSize='cover'
+              bgImage={post.media_url}
+              w='400px'
+              h='400px'
+            />{' '}
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </>
+  );
 };
