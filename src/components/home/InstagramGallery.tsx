74b6713806585036f643587a2d0f1c2b9035d941
Zak Mayfield <zakmayfield@gmail.com>
2023-04-30 11:46:38 -0400

    added Img's to sliders

--- src/components/home/InstagramGallery.tsx
+++ src/components/home/InstagramGallery.tsx
@@ -1,41 +1,54 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Pagination } from 'swiper';
-import { Box } from '@chakra-ui/react';
+import { Pagination, Navigation } from 'swiper';
+import { Box, useMediaQuery, Image } from '@chakra-ui/react';
 import { useGalleryContext } from '@/context/GalleryContext';
+import { ContentContainer } from '@/shared/components';
 import './swiper.css';
 import 'swiper/css';
 import 'swiper/swiper-bundle.css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
-export const InstagramGallery = () => {
+const IgSwiper = () => {
   const { data } = useGalleryContext();
+  const [isLargerThanTablet] = useMediaQuery('(min-width: 768px)');
 
   return (
-    <>
-      <Swiper
-        slidesPerView={3}
-        spaceBetween={5}
-        pagination={{
-          clickable: true,
-          dynamicBullets: true,
-        }}
-        modules={[Pagination]}
-      >
-        {data.map((post) => (
-          <SwiperSlide key={post.id}>
-            {' '}
-            <Box
-              bgPosition='center'
-              bgRepeat='no-repeat'
-              bgSize='cover'
-              bgImage={post.media_url}
-              w='400px'
-              h='400px'
-            />{' '}
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </>
+    <Swiper
+      modules={[Pagination, Navigation]}
+      slidesPerView={isLargerThanTablet ? 5 : 2}
+      pagination={{
+        clickable: true,
+        dynamicBullets: true,
+      }}
+      navigation
+    >
+      {data.map((post) => (
+        <SwiperSlide key={post.id} style={{ border: '1px solid black' }}>
+          {' '}
+          {/* <Box
+            bgPosition='center'
+            bgRepeat='no-repeat'
+            bgSize='cover'
+            bgImage={post.media_url}
+            w='250px'
+            h='250px'
+          />{' '} */}
+          <Box>
+            <Image src={post.media_url} alt='instagram post by chasers juice' />
+          </Box>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+};
+
+export const InstagramGallery = () => {
+  return (
+    <Box>
+      <ContentContainer maxW='container.2xl' my='8' px='12'>
+        <IgSwiper />
+      </ContentContainer>
+    </Box>
   );
 };
