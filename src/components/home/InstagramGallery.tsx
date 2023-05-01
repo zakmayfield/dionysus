2144e5620506e3e3892691599360088894bcae7a
Zak Mayfield <zakmayfield@gmail.com>
2023-05-01 12:27:10 -0400

    react-slick

--- src/components/home/InstagramGallery.tsx
+++ src/components/home/InstagramGallery.tsx
@@ -1,52 +1,50 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Pagination, Navigation } from 'swiper';
-import { Box, useMediaQuery, Image } from '@chakra-ui/react';
+import { Box, useMediaQuery, Image, Flex } from '@chakra-ui/react';
+import Slider from 'react-slick';
 import { useGalleryContext } from '@/context/GalleryContext';
 import { ContentContainer } from '@/shared/components';
-import './swiper.css';
-import 'swiper/css';
-import 'swiper/swiper-bundle.css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 const IgSwiper = () => {
   const { data } = useGalleryContext();
   const [isLargerThanTablet] = useMediaQuery('(min-width: 768px)');
 
+  const sliderSettings = {
+    dots: false,
+    infinite: true,
+    slidesToShow: isLargerThanTablet ? 4 : 2,
+    slidesToScroll: isLargerThanTablet ? 3 : 2,
+    autoplay: true,
+    speed: 500,
+    autoplaySpeed: 5000,
+    cssEase: 'linear',
+    pauseOnHover: true,
+    arrows: false,
+  };
+
   return (
-    <Swiper
-      modules={[Pagination, Navigation]}
-      slidesPerView={isLargerThanTablet ? 5 : 2}
-      pagination={{
-        clickable: true,
-        dynamicBullets: true,
-      }}
-      navigation
-    >
-      {data.map((post) => (
-        <SwiperSlide key={post.id} style={{ border: '1px solid black' }}>
-          {' '}
-          {/* <Box
-            bgPosition='center'
-            bgRepeat='no-repeat'
-            bgSize='cover'
-            bgImage={post.media_url}
-            w='250px'
-            h='250px'
-          />{' '} */}
-          <Box>
-            <Image src={post.media_url} alt='instagram post by chasers juice' />
-          </Box>
-        </SwiperSlide>
-      ))}
-    </Swiper>
+    <Box>
+      <Slider {...sliderSettings}>
+        {data.map((post) => (
+          <Flex key={post.id}>
+            <Image
+              boxSize='xs'
+              fit='scale-down'
+              src={post.media_url}
+              alt='Instagram Post by @chasersjuicetoronto'
+              mx='auto'
+            />
+          </Flex>
+        ))}
+      </Slider>
+    </Box>
   );
 };
 
 export const InstagramGallery = () => {
   return (
     <Box>
-      <ContentContainer maxW='container.2xl' my='8' px='12'>
+      <ContentContainer maxW='container.2xl' py='12'>
         <IgSwiper />
       </ContentContainer>
     </Box>
