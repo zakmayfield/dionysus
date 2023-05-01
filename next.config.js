785f9904ca72a876db77d167accc06c33e40d5ba
Zak Mayfield <zakmayfield@gmail.com>
2023-05-01 12:29:19 -0400

    edit config

--- next.config.js
+++ next.config.js
@@ -3,16 +3,6 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-  // images: {
-  //   remotePatterns: [
-  //     {
-  //       protocol: 'https',
-  //       hostname: 'scontent-yyz1-1.cdninstagram.com',
-  //       port: '',
-  //       pathname: '/account123/**',
-  //     },
-  //   ],
-  // },
 };
 
 module.exports = nextConfig;
