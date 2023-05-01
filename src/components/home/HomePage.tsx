0a7332e15b2d7a6818021a08e7933b13c619627f
Dani Mayfield <dani@neon.cc>
2023-05-01 19:54:21 -0400

    feat: add instagram gallery to home page

--- src/components/home/HomePage.tsx
+++ src/components/home/HomePage.tsx
@@ -3,6 +3,7 @@ import { Box } from '@chakra-ui/react';
 import HomeHero from './HomeHero';
 import HomeStatement from './HomeStatement';
 import HomeNarrativeHub from './HomeNarrativeHub';
+import InstagramGallery from './InstagramGallery';
 
 export default function HomePage() {
   return (
@@ -10,6 +11,7 @@ export default function HomePage() {
       <HomeHero />
       <HomeStatement />
       <HomeNarrativeHub />
+      <InstagramGallery />
     </Box>
   );
 }
