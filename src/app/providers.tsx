52a47200799f19f04acbd5f2739bd214eb35aed1
Dani Mayfield <dani@neon.cc>
2023-05-02 15:00:29 -0400

    chore: add new font

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -7,6 +7,7 @@ import '@fontsource/poppins/300.css';
 import '@fontsource/poppins/400.css';
 import '@fontsource/poppins/600.css';
 import '@fontsource/poppins/700.css';
+import '@fontsource/londrina-outline';
 
 export function Providers({ children }: PropsWithChildren) {
   return (
