f2c4e2519b94f24b7cdb766d255dd628fe4c7723
Dani Mayfield <dani@neon.cc>
2023-05-01 20:00:20 -0400

    chore: add server-only

--- src/shared/utils/getInstagramData.ts
+++ src/shared/utils/getInstagramData.ts
@@ -1,3 +1,5 @@
+import 'server-only';
+
 export const getInstagramData = async () => {
   const response = await fetch(
     `https://graph.instagram.com/me/media?fields=id,caption,media_url,media_type,permalink&access_token=${process.env.IG_KEY}`
