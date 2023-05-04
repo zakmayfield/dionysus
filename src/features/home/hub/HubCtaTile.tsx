0b729af886dd197865608ad5969b0d8bf6498944
Dani Mayfield <dani@neon.cc>
2023-05-04 17:51:55 -0400

    chore: update all links to correct routes

--- src/features/home/hub/HubCtaTile.tsx
+++ src/features/home/hub/HubCtaTile.tsx
@@ -4,9 +4,10 @@ import NextLink from 'next/link';
 type HubCtaTileProps = {
   title: string;
   content: string;
+  route: string;
 };
 
-export default function HubCtaTile({ title, content }: HubCtaTileProps) {
+export default function HubCtaTile({ title, content, route }: HubCtaTileProps) {
   return (
     <Flex
       flexFlow='column'
@@ -22,7 +23,7 @@ export default function HubCtaTile({ title, content }: HubCtaTileProps) {
       <Text my='5'>{content}</Text>
 
       <Box>
-        <Button as={NextLink} href='.'>
+        <Button as={NextLink} href={route}>
           Learn More
         </Button>
       </Box>
--- src/features/home/hub/NarrativeHub.tsx
+++ src/features/home/hub/NarrativeHub.tsx
@@ -2,6 +2,7 @@ import { Box, Flex } from '@chakra-ui/react';
 import HubCtaTile from './HubCtaTile';
 import lemonade from '~shared/assets/compressed/lemonade3.jpeg';
 import pom from '~shared/assets/compressed/pom1.jpeg';
+import routes from '@/shared/routes';
 /*
     Layout
 
@@ -39,6 +40,8 @@ export default function NarrativeHub() {
           <HubCtaTile
             title='Juiced then Delivered'
             content='We begin each day at 7am creating all of our juice orders so we can deliver to our Toronto customers that very same day. Check our delivery map to see if we deliver to your area.'
+            // TODO: update this route to appropriate route when created
+            route={routes.about}
           />
         </Flex>
       </Flex>
@@ -62,6 +65,8 @@ export default function NarrativeHub() {
           <HubCtaTile
             title='No Waste'
             content='We are committed to finding new and innovative ways to limit the amount of waste we produce by using as much of each fruit as possible. For the small parts we cannot use, have sourced a company that recycles and renews biological waste into electrical energy.'
+            // TODO: update this route to appropriate route when created
+            route={routes.about}
           />
         </Flex>
       </Flex>
