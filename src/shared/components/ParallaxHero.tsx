c256f57ab317fac49b720727c77b89e9888e8a85
Dani Mayfield <dani@neon.cc>
2023-05-05 17:57:26 -0400

    feat: create the parallax hero section on home page

---
+++ src/shared/components/ParallaxHero.tsx
@@ -0,0 +1,96 @@
+import { useScroll, useSpring, useTransform } from 'framer-motion';
+import { Box, BoxProps } from '@chakra-ui/react';
+import React, { useLayoutEffect, useRef, useState } from 'react';
+import { ContentContainer } from './ContentContainer';
+import { MotionBox, MotionBoxProps } from './motion-chakra';
+
+type ParallaxHeroProps = React.PropsWithChildren & {
+  bgImgSrc: string;
+  showOverlay?: boolean;
+  overlayColor?: string;
+  /** Props to be passed to the image */
+  imageProps?: Pick<
+    MotionBoxProps,
+    | 'backgroundPosition'
+    | 'backgroundRepeat'
+    | 'backgroundSize'
+    | 'backgroundAttachment'
+    | 'backgroundColor'
+  >;
+  /** This prop determines where the child element is positioned within the
+   * hero
+   * */
+  childOffsetPosition?: BoxProps['top'];
+};
+
+export const ParallaxHero = ({
+  bgImgSrc,
+  showOverlay = false,
+  overlayColor = 'whiteAlpha.300',
+  childOffsetPosition = { sm: '-28rem', base: '-96' },
+  imageProps,
+  children,
+}: ParallaxHeroProps) => {
+  const { scrollY } = useScroll();
+  const ref = useRef<HTMLElement>(null);
+  const [offsetTop, setOffsetTop] = useState(0);
+  const springConfig = {
+    stiffness: 20,
+    mass: 3,
+  };
+
+  useLayoutEffect(() => {
+    const onResize = () => {
+      if (ref.current) {
+        setOffsetTop(ref.current.offsetTop);
+      }
+    };
+    onResize();
+    window.addEventListener('resize', onResize);
+
+    return () => window.removeEventListener('resize', onResize);
+  }, [ref]);
+
+  const y = useSpring(
+    useTransform(scrollY, [offsetTop - 500, offsetTop + 500], ['0%', '70%']),
+    springConfig
+  );
+
+  const height = '65vh';
+  const heightUnit = height.match(/[a-z]+/gi)?.join('');
+  const heightNumbers = Number(height.match(/\d+/g)?.join(''));
+
+  return (
+    <ContentContainer maxW='container.2xl' h={height} overflow='hidden' px='0'>
+      <MotionBox
+        ref={ref}
+        initial={{ y: 0 }}
+        style={{ y, scale: 1.15 }}
+        py={`${heightNumbers / 2}${heightUnit}`}
+        backgroundColor='white'
+        backgroundImage={bgImgSrc}
+        backgroundPosition='center'
+        backgroundRepeat='no-repeat'
+        backgroundSize='cover'
+        position='relative'
+        {...imageProps}
+      >
+        {showOverlay && (
+          <Box
+            position='absolute'
+            top='0'
+            left='0'
+            bg={overlayColor}
+            w='full'
+            h='full'
+          />
+        )}
+      </MotionBox>
+      <Box position='relative' zIndex={10}>
+        <Box w='full' position='absolute' zIndex={30} top={childOffsetPosition}>
+          {children}
+        </Box>
+      </Box>
+    </ContentContainer>
+  );
+};
