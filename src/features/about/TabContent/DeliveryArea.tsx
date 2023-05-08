4133e38f299d3ff822fe91cedff8137ca2e12b5b
Zak Mayfield <zakmayfield@gmail.com>
2023-05-08 12:09:52 -0400

    updating styles

--- src/features/about/TabContent/DeliveryArea.tsx
+++ src/features/about/TabContent/DeliveryArea.tsx
@@ -1,4 +1,12 @@
-import { Box, Flex, Text } from '@chakra-ui/react';
+import {
+  Box,
+  Text,
+  Stack,
+  Heading,
+  List,
+  ListItem,
+  Flex,
+} from '@chakra-ui/react';
 import { ContentContainer } from '@/shared/components';
 import map from '@/shared/assets/about/delivery-area-map.jpeg';
 
@@ -6,7 +14,7 @@ export const DeliveryArea = () => {
   return (
     <Box as='section'>
       {/* delivery area intro */}
-      <ContentContainer maxW='container.md' letterSpacing='wide' my='12'>
+      <ContentContainer maxW='container.lg' letterSpacing='wide' my='16'>
         <DeliveryInfo />
       </ContentContainer>
 
@@ -20,28 +28,59 @@ export const DeliveryArea = () => {
 
 function DeliveryInfo() {
   return (
-    <>
-      <Text fontSize={{ base: 'md', md: 'lg' }}>
-        Our main areas we deliver are within the downtown core of Toronto and
-        the surrounding neighbourhoods.
-      </Text>
-      <Text fontSize={{ base: 'md', md: 'lg' }} mt='5' mb='10'>
-        We also deliver within the GTA as well as outside of the GTA and all
-        over Canada!
-      </Text>
-      <Text fontSize={{ base: 'md', md: 'lg' }}>
-        Don’t hesitate to contact us if you’re outside of our core delivery zone
-        as it is possible for us to make special arrangements to deliver to your
-        location.
-      </Text>
-    </>
+    <Box>
+      <Stack
+        direction={{ base: 'column', md: 'row' }}
+        spacing={{ base: '6', md: '10' }}
+        justify='center'
+        align={{ base: 'center', md: 'flex-start' }}
+        px={{ base: '4', md: '0' }}
+        textAlign='left'
+      >
+        <Box w={{ base: 'full', md: '60%' }}>
+          <Text
+            fontSize={{ base: 'md', md: 'lg' }}
+            lineHeight={{ base: 'taller', md: 'tall' }}
+            mb={{ base: '4', md: '6' }}
+          >
+            Our main areas we deliver are within the downtown core of Toronto
+            and the surrounding neighbourhoods.
+          </Text>
+          <Text
+            fontSize={{ base: 'md', md: 'lg' }}
+            lineHeight={{ base: 'taller', md: 'tall' }}
+            mb={{ base: '4', md: '0' }}
+          >
+            We also deliver within the GTA as well as outside of the GTA and all
+            over Canada!
+          </Text>
+        </Box>
+
+        <Box
+          w={{ base: 'full', md: '40%' }}
+          borderRadius='lg'
+          p='8'
+          bg='tertiary.200'
+        >
+          <Text
+            fontSize={{ base: 'sm', md: 'md' }}
+            lineHeight={{ base: 'taller', md: 'tall' }}
+            mb={{ base: '4', md: '0' }}
+          >
+            Don’t hesitate to contact us if you’re outside of our core delivery
+            zone as it is possible for us to make special arrangements to
+            deliver to your location.
+          </Text>
+        </Box>
+      </Stack>
+    </Box>
   );
 }
 
 function MapContainer() {
   return (
     <Flex
-      minH='60vh'
+      minH='70vh'
       justifyContent='center'
       alignItems='center'
       flexFlow='column'
