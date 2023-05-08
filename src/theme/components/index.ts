29c3a2900dea3ea262d7c2bf921e656d0f41bc45
Dani Mayfield <dani@neon.cc>
2023-05-08 16:15:52 -0400

    chore: customize the link component

--- src/theme/components/index.ts
+++ src/theme/components/index.ts
@@ -1,7 +1,10 @@
 import { buttonTheme } from './button';
+import { linkTheme } from './link';
 
 export { buttonTheme as Button } from './button';
+export { linkTheme as Link } from './link';
 
 export const components = {
   Button: buttonTheme,
+  Link: linkTheme,
 };
