2243307340e62db78a6cac6a2bc23153ea6b0546
Zak Mayfield <zakmayfield@gmail.com>
2023-05-16 15:55:44 -0400

    remove unused imports

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,6 @@
-// import { Hero } from '@/shared/components';
 export default function About() {
   return (
     <section>
-      {/* <Hero title='test' imgSrc='test' /> */}
       <div>redirecting to /about-us</div>
     </section>
   );
