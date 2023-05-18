589e02d32eafd0ff57c5965f98cc9a639574eda8
Dani Mayfield <dani@neon.cc>
2023-05-18 16:06:25 -0400

    chore: update functionality of active navigation state

--- src/shared/routes.ts
+++ src/shared/routes.ts
@@ -8,8 +8,7 @@ export default {
   products: '/products',
   faq: '/faq',
   contact: '/contact',
-  about: aboutRoute('/our-story'),
-  philosophy: aboutRoute('/philosophy'),
-  wholesale: aboutRoute('/wholesale'),
-  deliveryArea: aboutRoute('/delivery-area'),
+  about: '/about',
+  philosophy: aboutRoute('philosophy'),
+  deliveryArea: aboutRoute('delivery-area'),
 } as const;
