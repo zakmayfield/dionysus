6fc1a55f657696549e008d4ed1bb77ab47f6c640
Dani Mayfield <dani@neon.cc>
2023-05-19 16:15:41 -0400

    feat: create not found page with juice image

---
+++ src/features/notFound/NotFoundPage.tsx
@@ -0,0 +1,74 @@
+'use client';
+import { Box, Button, Flex, Heading, Text } from '@chakra-ui/react';
+import React from 'react';
+import NextImage from 'next/image';
+import { useRouter } from 'next/navigation';
+import FeaturedJuice from '@/shared/assets/notFound/centre-juice-lcp.png';
+
+export const NotFoundPage = () => {
+  const router = useRouter();
+  return (
+    <Box
+      w='full'
+      minH={{ md: '900px', sm: '700px', base: '500px' }}
+      position='relative'
+      display='flex'
+      justifyContent='center'
+      alignItems='center'
+      mx='auto'
+      maxW='container.2xl'
+    >
+      {/* Orange circle */}
+      <Box w='750px' h='750px' bg='rgb(248,206,88)' rounded='full' />
+      <Flex
+        position='absolute'
+        top='0'
+        direction='column'
+        justifyContent='center'
+        alignItems='center'
+      >
+        <Heading
+          as='h1'
+          textTransform='uppercase'
+          fontFamily='Londrina Outline'
+          letterSpacing='widest'
+          fontSize={{ lg: '250px', sm: '200px', base: '150px' }}
+          lineHeight='none'
+          fontWeight='light'
+          textAlign='center'
+          color='blackAlpha.800'
+          w='full'
+        >
+          404
+        </Heading>
+        <Box position='absolute' top='18vh' transform='rotate(10deg)'>
+          <NextImage
+            src={FeaturedJuice.src}
+            alt='Bottle of chasers juice lychee cactus pear juice'
+            width={FeaturedJuice.width / 1.3}
+            height={FeaturedJuice.height / 1.3}
+            quality={100}
+            style={{
+              filter: 'drop-shadow(30px 15px 4px rgba(0, 0, 0, 0.08))',
+            }}
+          />
+        </Box>
+      </Flex>
+      <Box
+        position='absolute'
+        maxW='prose'
+        left='0'
+        px={{ md: '12', base: '6' }}
+      >
+        <Heading>Oops!</Heading>
+        <Heading>Page not found</Heading>
+        <Text mt='3'>
+          It looks like the page you&apos;re trying to visit doesn&apos;t exist.
+        </Text>
+        <Button onClick={() => router.back()} variant='outline' mt='3'>
+          Go back
+        </Button>
+      </Box>
+    </Box>
+  );
+};
