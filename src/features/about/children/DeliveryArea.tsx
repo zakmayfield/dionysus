0c47b85139d40cf730e6de2ab15d176be175cb66
Zak Mayfield <zakmayfield@gmail.com>
2023-05-19 12:40:08 -0400

    added fade in animation to delivery

--- src/features/about/children/DeliveryArea.tsx
+++ src/features/about/children/DeliveryArea.tsx
@@ -2,14 +2,18 @@
 
 import { Box } from '@chakra-ui/react';
 import { DeliveryInfo, MapContainer } from './delivery';
-import { ContentContainer } from '@/shared/components';
+import { ContentContainer, FadeInBox } from '@/shared/components';
 
 export const DeliveryArea = () => {
   return (
     <Box as='section' mt='6' mb='12'>
       <ContentContainer maxW='container.lg' letterSpacing='wide' px='6'>
-        <DeliveryInfo />
-        <MapContainer />
+        <FadeInBox>
+          <DeliveryInfo />
+        </FadeInBox>
+        <FadeInBox>
+          <MapContainer />
+        </FadeInBox>
       </ContentContainer>
     </Box>
   );
