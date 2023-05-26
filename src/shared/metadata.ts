fac76c365808f2e317d4d6959ce61f15e295cde7
Dani Mayfield <dani@neon.cc>
2023-05-26 14:29:42 -0400

    chore: adding icons to metadata & updating viewport

--- src/shared/metadata.ts
+++ src/shared/metadata.ts
@@ -1,4 +1,6 @@
-export const sharedMetadata = {
+import { Metadata } from 'next';
+
+export const sharedMetadata: Metadata = {
   title: 'Chasers Fresh Juice Inc.',
   description: 'Toronto based wholesale fresh juice company',
   keywords: [
@@ -33,6 +35,27 @@ export const sharedMetadata = {
   viewport: {
     width: 'device-width',
     initialScale: 1,
-    maximumScale: 1,
+  },
+  icons: {
+    icon: [
+      { rel: 'icon', url: '/favicon.ico' },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        url: '/favicon-32x32.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        url: '/favicon-16x16.png',
+      },
+    ],
+    apple: {
+      rel: 'apple-touch-icon',
+      url: '/apple-touch-icon.png',
+      sizes: '180x180',
+    },
   },
 };
