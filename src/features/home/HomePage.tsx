b928867e8b68f7682449a29863f266dfb7be9cee
Zak Mayfield <zakmayfield@gmail.com>
2023-07-24 12:08:43 -0400

    comment out ig feed

--- src/features/home/HomePage.tsx
+++ src/features/home/HomePage.tsx
@@ -16,7 +16,7 @@ export default function HomePage() {
       </FadeInBox>
       <HomeNarrativeHub />
       <ProductsHero />
-      <InstagramGallery />
+      {/* <InstagramGallery /> */}
     </Box>
   );
 }
